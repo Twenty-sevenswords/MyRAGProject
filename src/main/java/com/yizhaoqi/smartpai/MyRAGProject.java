0b65de1b7e3daf5030f4eb1ab72015ba5ec7067d
ershiqijian <2868227296@qq.com>
2026-02-12 14:54:51 +0800

    项目提交

--- src/main/java/com/yizhaoqi/smartpai/MyRAGProject.java
+++ src/main/java/com/yizhaoqi/smartpai/MyRAGProject.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyRAGProject {
-
     public static void main(String[] args) {
-        SpringApplication.run(SmartPaiApplication.class, args);
+        SpringApplication.run(MyRAGProject.class, args);
     }
 
 }
